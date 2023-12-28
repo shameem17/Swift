39215d70ece1a3ea676f969ae8830da17c4c52e9
shameem17 <shameem105455@gmail.com>
2023-12-28 10:41:39 +0600

    Dictionary added

---
+++ Collections/Dictionary.swift
@@ -0,0 +1,52 @@
+//initialization
+print("-----Initialization------>")
+// key, value
+
+var userInfo: Dictionary<Int, String> = [:]
+var phoneNUmber: [Int: [String]] = [:]
+userInfo = [1: "b", 2: "a"]
+userInfo[102] = "c"
+print(userInfo)
+
+print("-----old value------>")
+
+
+if let oldValue = userInfo.updateValue("d", forKey: 1){
+    print("oldValue = \(oldValue) replaced by \(userInfo[1]!)")
+}
+
+print("------remove--------")
+userInfo[2] = nil //forKey 2 will be removed
+print("after removing 2:",userInfo)
+
+userInfo.removeValue(forKey: 102)
+print("removing 102:",userInfo)
+
+print("-------iteration------->")
+//inserting some new value
+userInfo[2] = "c"
+userInfo[3] = "d"
+userInfo[4] = "e"
+userInfo[5] = "f"
+userInfo[6] = "g"
+userInfo[7] = "h"
+userInfo[8] = "i"
+//dictionary is unordered
+for (key,value) in userInfo{
+     
+    print("key=\(key), value= \(value)")
+}
+
+//sorted by key
+
+for key in userInfo.keys.sorted() {
+    print("key=\(key)")
+}
+
+//sorted by value
+
+for value in userInfo.values.sorted() {
+    print("key=\(value)")
+}
+
+//keys are hashable
