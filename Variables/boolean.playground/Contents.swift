ad14fd2c9cb9736cb9ff939d902916aed7e4ed19
shameem17 <shameem105455@gmail.com>
2024-01-02 15:57:11 +0600

    Variables

---
+++ Variables/boolean.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+//boolean diclaration
+
+let isActive = false
+let isStudent = true
+print(isActive)
+print(isStudent)
+
+//var isAuthenticated = 1 //not a boolean type
+//Int type variable
+
+
+//if isAuthenticated{
+//    print("Authenticated")
+//}
+
+//function return boolean values
+
+var isMultiple = 120.isMultiple(of: 4)
+
+if isMultiple{
+    print("120 is multiple of 4")
+}
+
+//toggle
+print("isMultiple= \(isMultiple)")
+isMultiple = !isMultiple
+print("isMultiple= \(isMultiple)")
+isMultiple.toggle()
+print("isMultiple= \(isMultiple)")
---
+++ Variables/int.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Cocoa
+
+//normal int
+
+let score = 10
+
+//big int
+/// use as (,) format like 1,000,000...
+
+let bigInt = 10_000_000_000
+
+//swift ignore (_). _ can be any number
+print(bigInt)
+
+var phoneNumber = 1____788____381_679
+
+print(phoneNumber)
+phoneNumber += 1
+print(phoneNumber)
+
+//type casting
+
+let a = 10
+let b = 31.3
+let f = Int(34.999999)
+let result = a + Int(b)
+print(result)
